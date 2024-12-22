d269824ed16118b8f503d5091cf5eb35271007e6
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-12-22 22:02:05 +0530

    Landing Page completed

--- portfolio/src/Sections/Home.jsx
+++ portfolio/src/Sections/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
-import bg from '../assets/Images/2040596_275310-P5VU87-469.jpg'
+import React from "react";
+import bg from "../assets/Images/2040596_275310-P5VU87-469.jpg";
+import astronaut from "../assets/Images/rb_38616.png";
+import Typewriter from "typewriter-effect";
 const Home = () => (
-  <div id="home" className="section bg-blue-500 text-white flex items-center justify-center">
-    <img src={bg} alt="" />
-    
+  <div className="relative min-h-screen w-full overflow-hidden group">
+    <div className="absolute inset-0 w-full h-full transition-all duration-500 group-hover:blur-sm">
+      <img
+        src={bg}
+        alt="Cloud Background"
+        className="w-full h-full object-cover"
+      />
+    </div>
+
+    <div className="relative z-10 min-h-screen px-4 flex items-center justify-center">
+      <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl w-full gap-8">
+        <div className="text-center md:text-left flex-1">
+          <h1 className="flex gap-2 text-4xl md:text-6xl font-bold mb-4 text-white">
+            <span>Hi, I am</span>
+            <Typewriter
+              options={{
+                strings: ['Aviral Asthana'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+
+          <h2 className="text-2xl md:text-4xl mb-4">
+            <span className="text-gray-400">
+              Back-End / Full Stack Developer
+            </span>
+          </h2>
+          <p className="text-lg md:text-xl max-w-2xl text-fuchsia-200">
+            I am a Back-End / Full-Stack Developer. I have experience in
+            developing web applications using Node.js, Express.js, and React.js.
+          </p>
+        </div>
+
+        <div className="flex">
+          <img
+            src={astronaut}
+            alt="Astronaut"
+            className="w-[500px] h-[500px] object-contain hover:translate-x-4 hover:translate-y-4 transition-transform duration-500"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
